65d6f29dfc5c4162305d1ed998e9717f3f2d40b8
kodjoSoule <souleymanekodjo@gmail.com>
2024-01-15 09:00:05 +0000

    ajout des components principale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FashionProductService } from './services/fashion-product.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  providers: [FashionProductService],
+  imports: [CommonModule, RouterOutlet, HomeComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+import { HomeComponent } from './components/home/home.component';
+import { LoginUserComponent } from './components/authentication/login-user/login-user.component';
+// import { RegisterComponent } from './components/authentication/register/register.component';
+// import { ProductListComponent } from './components/product-list/product-list.component';
+// import { ProductDetailsComponent } from './components/product-details/product-details.component';
+// import { CartComponent } from './components/cart/cart.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RegisterUserComponent } from './components/authentication/register-user/register-user.component';
+import { FashionProductListComponent } from './components/fashion-products/fashion-product-list/FashionProductListComponent';
+import { FashionProductItemComponent } from './components/fashion-products/fashion-product-item/fashion-product-item.component';
+import { FashionProductDetailsComponent } from './components/fashion-products/fashion-product-details/fashion-product-details.component';
+import { FashionProductsOnSaleComponent } from './components/fashion-products/fashion-products-on-sale/fashion-products-on-sale.component';
+import { CategoriesComponent } from './components/fashion-products/categories/categories.component';
+import { ProductsByCategoryComponent } from './components/fashion-products/products-by-category/products-by-category.component';
+
+
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', title: 'Home Page', component: HomeComponent },
+    { path: 'login', title: 'Page de Connexion', component: LoginUserComponent },
+    { path: 'register', title: 'Page d\'inscription', component: RegisterUserComponent },
+    { path: 'products', title: 'Listes des produits', component: FashionProductListComponent },
+    { path: 'products/:id', title: 'Detail d\'un produit', component: FashionProductDetailsComponent },
+    { path : 'onSale', title: 'Listes des produits en promotion', component: FashionProductsOnSaleComponent },
+    {path : 'categories', title: 'Listes des produits par catégorie', component: CategoriesComponent},
+    { path: 'products/categories/:id', title: 'Product Details', component: ProductsByCategoryComponent },
+    { path: '**', component: NotFoundComponent }
+];
